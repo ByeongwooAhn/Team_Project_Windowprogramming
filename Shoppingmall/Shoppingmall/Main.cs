30c8ac24474049acd65a44f5881b078a8d9aef34
abyungwoo <abyungwoo@gmail.com>
2022-11-15 21:09:01 +0900

    쇼핑몰의 프로토타입을 제작 중 입니다.

---
+++ Shoppingmall/Shoppingmall/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shoppingmall
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_butto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
