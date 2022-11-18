a47dd6333e40fbc54536526d3a0a3d656daab2b5
khyejin27 <khyejin816@naver.com>
2022-11-18 15:06:35 +0900

    옷으로 그림을 바꾸고 로그인 여부에 따라 결제창 또는 비회원 창이 뜨드록 하였습니다.

--- Shoppingmall/Shoppingmall/Main.cs
+++ Shoppingmall/Shoppingmall/Main.cs
@@ -23,7 +23,7 @@ namespace Shoppingmall
             login.ShowDialog();
         }
 
-        private void Price1_Click(object sender, EventArgs e)
+     /*   private void Price1_Click(object sender, EventArgs e)
         {
             Pay pay = new Pay();
 
@@ -41,21 +41,7 @@ namespace Shoppingmall
                 pay.ShowDialog();
             }
         }
-
-        private void Login_button_MouseDown(object sender, MouseEventArgs e)
-        {
-            /* if(e.Button== MouseButtons.Right)
-            {
-                DialogResult result = MessageBox.Show("로그아웃 하시겠습니까?","알림", MessageBoxButtons.YesNo);
-                if(result == DialogResult.Yes) 
-                {
-                    MessageBox.Show("로그아웃되었습니다.");
-                    Login_button.Text = "Login";
-                }
-
-            }*/
-        }
-
+     */
         private void 내정보ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("내 정보");
@@ -69,6 +55,468 @@ namespace Shoppingmall
                 MessageBox.Show("로그아웃되었습니다.");
                 Login_button.Text = "Login";
             }
+        }
+
+        private void label2_Click(object sender, EventArgs e)  //옷 가격 1
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)  //옷 사진1
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            #region 
+            Pay pay = new Pay();
+
+            if (Login_button.Text == "Login")
+            {
+                DialogResult result = MessageBox.Show("현재 비회원 상태입니다.\n비회원으로 주문하시겠습니까?", "알림", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    pay.ShowDialog();
+                }
+            }
+            else
+            {
+                pay.ShowDialog();
+            }
+            #endregion
         }
     }
 }
